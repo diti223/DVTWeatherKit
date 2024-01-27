27d832b5dab184bd607f3bb651b12d34bd0dfb2b
Adrian Bilescu <adrian.bilescu@gmail.com>
2024-01-27 16:36:20 +0200

    Implemented forecast days initial values.

--- Tests/DVTWeatherKitTests/WeatherViewModelTests.swift
+++ Tests/DVTWeatherKitTests/WeatherViewModelTests.swift
@@ -11,13 +11,41 @@ struct WeatherViewModel {
     let currentTemperature = "-°"
     let minTemperature = "-°"
     let maxTemperature = "-°"
+    
+    let forecastDays = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    let date: Date
+    
+    init(date: Date = Date()) {
+        self.date = date
+    }
 }
 
-final class WeatherViewModelTests {
+final class WeatherViewModelTests: XCTestCase {
     func testOnInit() {
         let sut = WeatherViewModel()
         XCTAssertEqual(sut.currentTemperature, "-°")
         XCTAssertEqual(sut.minTemperature, "-°")
         XCTAssertEqual(sut.maxTemperature, "-°")
+        
+    }
+    
+    func testOnInitPresentForecastDays() {
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponents = DateComponents(
+            calendar: calendar,
+            timeZone: .current,
+            year: 2024,
+            month: 1,
+            day: 29,
+            hour: 1,
+            minute: 1
+        )
+        let date = calendar.date(from: dateComponents)!
+        
+        XCTAssertEqual(
+            WeatherViewModel(date: date).forecastDays,
+            ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        )
     }
 }
